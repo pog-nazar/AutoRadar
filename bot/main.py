670fc65fbcae0f11814ebb6d44e129fe2a38ed24
admin <admin@admins-MacBook-Pro.local>
2024-07-02 10:19:14 +0300

    First release

---
+++ bot/main.py
@@ -0,0 +1,23 @@
+from telethon import TelegramClient, events
+from bot.data import data
+from sender import send_message_to_channel
+
+api_id = data["api_id"]
+api_hash = data["api_hash"]
+channel = "@air_alert_ua"
+test_channel = "@testRadar2"
+
+client = TelegramClient("AutoRadar", api_id, api_hash)
+
+
+@client.on(events.NewMessage(chats=channel))
+async def handler(event):
+    await send_message_to_channel(event.message.text)
+
+
+async def main():
+    print("Client started. Listening for new messages...")
+    await client.run_until_disconnected()
+
+with client:
+    client.loop.run_until_complete(main())
---
+++ bot/sender.py
@@ -0,0 +1,19 @@
+from aiogram import Bot, Dispatcher
+from aiogram.utils import executor
+from bot.data import data
+import logging
+
+API_TOKEN = data["BOT_TOKEN"]
+CHANNEL_ID = "@testingRadar1"
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+async def send_message_to_channel(text: str):
+    await bot.send_message(CHANNEL_ID, text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
