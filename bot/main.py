9f34d04f7471caa638a8da38a2abfe7138f004f8
admin <admin@admins-MacBook-Pro.local>
2024-07-03 13:11:29 +0300

    7.0.2

--- bot/main.py
+++ bot/main.py
@@ -5,12 +5,11 @@ from sender import send_message_to_channel
 api_id = data["api_id"]
 api_hash = data["api_hash"]
 channel = "@air_alert_ua"
-test_channel = "testradar123"
 
 client = TelegramClient("AutoRadar", api_id, api_hash)
 
 
-@client.on(events.NewMessage(chats=test_channel))
+@client.on(events.NewMessage(chats=channel))
 async def handler(event):
     await send_message_to_channel(event.message.text)
 
